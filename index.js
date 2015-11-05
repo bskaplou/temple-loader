c51c7d141eac19d0eb8c84740c664fb919197737
Ainur <nur.php@gmail.com>
2015-11-05 17:16:24 +0700

    fix missing arguments

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ module.exports = function (content) {
   var name = loaderUtils.interpolateName(this, "[name]", {content: content});
   var parser = new DOMParser();
   var templates = {};
+  //TODO: add params support
+  var classes_preffix = '';
+  var no_modify_regex = /js/;
+  var drop_spaces = true;
 
   function collector(ins, source, arg1, arg2, arg3) {
     if (!templates.hasOwnProperty(source)) {
@@ -20,13 +24,16 @@ module.exports = function (content) {
     }
   }
 
-  content = content.replace(/>\s+</g, '><');
+  if (drop_spaces) {
+    content = content.replace(/>\s+</g, '><');
+  }
+
   node(name, 'root', parser.parseFromString(content), collector);
 
   var templates_code = [];
   for (var k in templates) {
     if (templates[k].length > 1) //Ignore stop instruction
-      templates_code.push('"' + k + '"' + ': function(pool){' + builder(templates[k]) + '}');
+      templates_code.push('"' + k + '"' + ': function(pool){' + builder(templates[k], classes_preffix, no_modify_regex) + '}');
   }
   return 'var temple_utils = require("temple-wat");' +
     'module.exports = { ' + templates_code.join(',') + '};';
